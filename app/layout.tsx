7fc21eb38d82fe598cd86ae6499f430d534d377d
vdpop <varadpuranik123@gmail.com>
2025-12-22 23:20:55 +0530

    Added @vercel/analytics dependency and integrated Analytics component into the layout for enhanced tracking and insights.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,6 +47,7 @@ export default function RootLayout({
         className={`${DepartureMono.variable} ${inter.variable} ${geistMono.variable} antialiased `}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
